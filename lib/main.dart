fe637f76ad1b00f60e3da0cd620909f623421572
dhandyjoe <dhanjoenkp@gmail.com>
2021-09-25 11:19:02 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-
-}
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Flutter jenny"),
+    ),
+    body: Center(
+      child: Text("flutter-basic-app"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text("Click"),
+    ),
+  ),
+));
 
