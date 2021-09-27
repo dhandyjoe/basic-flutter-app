80c9d304c520d036e990c95f5974e0321ac13900
dhandyjoe <dhanjoenkp@gmail.com>
2021-09-27 12:09:09 +0700

    add raised button

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Flutter jenny"),
-    ),
-    body: Center(
-      child: Text("flutter-basic-app"),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("Click"),
-    ),
-  ),
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print("Answer chosen!");
+  }
+
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter dasdasd"),
+        ),
+        body: Column (
+          children: [
+            Text("Question 1"),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 1"),
+            ),
+            RaisedButton(
+              onPressed: () => print("Answer 2 chosen!"),
+              child: Text("Answer 2"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 3"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 4"),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
 
