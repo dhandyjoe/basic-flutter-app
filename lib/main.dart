553b9e0a75ffde9a87c42e5c19dbac0f97ed1315
dhandyjoe <dhanjoenkp@gmail.com>
2021-10-02 16:31:29 +0700

    learn some widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,28 @@ class MyApp extends StatelessWidget {
             "Flutter",
           ),
         ),
-        body: const Center(
-          child: Image(
-            image: NetworkImage('https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'),
+        body: Center(child: Column(
+            children: [
+              Text('Dhandy Jenny'),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.print),
+                label: const Text('Print!'),
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10.0),
+                    primary: Colors.orangeAccent
+                ),
+              ),
+            ],
           ),
-        )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.lightBlue,
+        ),
       ),
     );
   }
 }
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
 
