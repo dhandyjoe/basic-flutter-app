27cca0303ed285ea33704098a516499186b19845
dhandyjoe <dhanjoenkp@gmail.com>
2021-10-02 08:33:08 +0700

    improve some widget, add network imagew

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,30 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void answerQuestion(){
-    print("Answer chosen!");
-  }
-
   @override
   Widget build (BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter dasdasd"),
+          title: const Text(
+            "Flutter",
+          ),
         ),
-        body: Column (
-          children: [
-            Text("Question 1"),
-            RaisedButton(
-              onPressed: answerQuestion,
-              child: Text("Answer 1"),
-            ),
-            RaisedButton(
-              onPressed: () => print("Answer 2 chosen!"),
-              child: Text("Answer 2"),
-            ),
-            RaisedButton(
-              onPressed: answerQuestion,
-              child: Text("Answer 3"),
-            ),
-            RaisedButton(
-              onPressed: answerQuestion,
-              child: Text("Answer 4"),
-            ),
-          ],
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'),
+          ),
         )
       ),
     );
   }
 }
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 
